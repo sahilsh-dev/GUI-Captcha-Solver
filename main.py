bd2fd8a6f04156252de661949330343d76b997ab
Sahil Sharma <vlsharma713@gmail.com>
2023-01-19 22:44:35 +0530

    Add Start Screen

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from tkinter import *
 
 
+def start():
+    print("Click")
+
+
 win = Tk()
 win.title("GUI Captcha Solver")
 win.minsize(width=700, height=500)
+win.config(bg="lightblue", padx=50, pady=50)
 
+icon_img = PhotoImage(file="icon.png")
+canvas = Canvas(win, width=258, height=194)
+canvas.create_image(128, 97, image=icon_img)
+canvas.pack(pady=20)
+
+start_btn = Button(text="Start", font=("Courier", 15, "normal"), command=start, width=11)
+start_btn.pack(pady=50)
 
 
 win.mainloop()
