02ad51a7418e2459f230dac982c34c69135e0255
Sahil Sharma <vlsharma713@gmail.com>
2023-01-20 23:40:28 +0530

    Update start screen

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from tkinter import *
 
 
-def start():
-    print("Click")
+def login():
+    login_screen.pack(fill='both', expand=True)
+    start_screen.pack_forget()
 
 
 win = Tk()
@@ -10,12 +11,18 @@ win.title("GUI Captcha Solver")
 win.minsize(width=700, height=500)
 win.config(bg="lightblue", padx=50, pady=50)
 
+start_screen = Frame(win)
+login_screen = Frame(win)
+start_screen.config(bg="lightblue")
+login_screen.config(bg="lightblue")
+
+start_screen.pack(fill=BOTH, expand=True)
 icon_img = PhotoImage(file="icon.png")
-canvas = Canvas(win, width=258, height=194)
+canvas = Canvas(start_screen, width=258, height=194)
 canvas.create_image(128, 97, image=icon_img)
 canvas.pack(pady=20)
 
-start_btn = Button(text="Start", font=("Courier", 15, "normal"), command=start, width=11)
+start_btn = Button(start_screen, text="Start", font=("Courier", 15, "normal"), command=login, width=11)
 start_btn.pack(pady=50)
 
 
