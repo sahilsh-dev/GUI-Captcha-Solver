09abb0dd9a2bb4758d6781a1f09beea49c2c8534
Sahil Sharma <vlsharma713@gmail.com>
2023-01-20 23:58:37 +0530

    Add login screen

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ login_screen = Frame(win)
 start_screen.config(bg="lightblue")
 login_screen.config(bg="lightblue")
 
+# Start Screen
 start_screen.pack(fill=BOTH, expand=True)
 icon_img = PhotoImage(file="icon.png")
 canvas = Canvas(start_screen, width=258, height=194)
@@ -24,6 +25,17 @@ canvas.pack(pady=20)
 
 start_btn = Button(start_screen, text="Start", font=("Courier", 15, "normal"), command=login, width=11)
 start_btn.pack(pady=50)
+
+# Login Screen
+uid_label = Label(login_screen, text="UID :")
+uid_label.pack(pady=10)
+uid = Entry(login_screen, width=20)
+uid.pack()
+
+pswd_label = Label(login_screen, text="Password :")
+pswd_label.pack(pady=10)
+pswd = Entry(login_screen, width=20)
+pswd.pack()
 
 
 win.mainloop()
