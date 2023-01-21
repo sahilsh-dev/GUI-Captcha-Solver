8ab813c26bd645cf90ccbdee2bf2e1bde26ee8c6
Sahil Sharma <vlsharma713@gmail.com>
2023-01-21 23:57:11 +0530

    Add login button

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from tkinter import *
 
 
-def login():
+def show_login_screen():
     login_screen.pack(fill='both', expand=True)
     start_screen.pack_forget()
+
+
+def login_cuims():
+    pass
 
 
 win = Tk()
@@ -14,7 +18,7 @@ win.config(bg="lightblue", padx=50, pady=50)
 start_screen = Frame(win)
 login_screen = Frame(win)
 start_screen.config(bg="lightblue")
-login_screen.config(bg="lightblue", pady=50)
+login_screen.config(bg="lightblue", pady=30)
 
 # Start Screen
 start_screen.pack(fill=BOTH, expand=True)
@@ -23,20 +27,30 @@ canvas = Canvas(start_screen, width=258, height=194)
 canvas.create_image(128, 97, image=icon_img)
 canvas.pack(pady=20)
 
-start_btn = Button(start_screen, text="Start", font=("Courier", 15, "bold"), command=login, bg='white', width=11)
+start_btn = Button(start_screen, text="Start", font=("Courier", 15, "bold"), command=show_login_screen, bg='crimson', width=11)
 start_btn.pack(pady=50)
 
 # Login Screen
-uid_label = Label(login_screen, text="UID :", font=("Courier", 15, "normal"), bg='white')
+uid_label = Label(login_screen, text="UID", font=("Courier", 15, "bold"), bg='crimson')
 uid_label.pack()
 uid = Entry(login_screen, width=18, font=("Courier", 13, "normal"))
 uid.pack(pady=20)
 
-pswd_label = Label(login_screen, text="Password :", font=("Courier", 15, "normal"), bg='white')
+pswd_label = Label(login_screen, text="Password", font=("Courier", 15, "bold"), bg='crimson')
 pswd_label.pack(pady=20)
 pswd = Entry(login_screen, width=18, font=("Courier", 13, "normal"))
-pswd.place(rely=0.5)
 pswd.pack()
 
+checked_state = IntVar()
+check_btn = Checkbutton(login_screen,
+                        text='Set Direct Login next time',
+                        variable=checked_state,
+                        font=("Modern", 11, "normal"),
+                        bg='white')
+
+check_btn.pack(pady=20)
+
+login_btn = Button(login_screen, text="Login CUIMS", font=("Courier", 15, "bold"), command=login_cuims, bg='crimson', width=11)
+login_btn.pack(pady=30)
 
 win.mainloop()
